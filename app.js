0a662242e25fbf2a62b763ff7446bbc72d5e54de
Swibm <swibm9009@gmalil.com>
2023-01-31 21:13:04 +0600

    changed request method

--- app.js
+++ app.js
@@ -7,52 +7,60 @@
 let data = []
 
 const requestCreation = () => {
-    const request = new XMLHttpRequest() //создание запроса
-    request.open("GET","data.json") // ОБъявление метода запроса. Open принимает метод запроса и его путь URL
-    request.setRequestHeader("Content-type", "application/json") //Задание заголовка
-    request.send()
-    request.addEventListener("load", () => {
-        console.log(request.response)
-        data = JSON.parse(request.response)
+    // const request = new XMLHttpRequest() //создание запроса
+    // request.open("GET","data.json") // ОБъявление метода запроса. Open принимает метод запроса и его путь URL
+    // request.setRequestHeader("Content-type", "application/json") //Задание заголовка
+    // request.send()
+    // request.addEventListener("load", () => {
+    //     console.log(request.response)
+    //     data = JSON.parse(request.response)
 
-        data.cars.map((item, i) => {
-            let price = item.price
-            let name = item.name
-            let link = item.URL
-            const card = document.createElement('div')
-            card.classList.add("card")
-            card.classList.add(`card${i}`)
-
-            const nameTag = document.createElement('div')
-            nameTag.classList.add("name")
-            nameTag.classList.add(`name${i}`)
-            nameTag.style.marginTop = "10px"
-            nameTag.style.fontSize = "20px"
-            nameTag.style.fontWeight = "700"
-            nameTag.style.color = "#fffced"
-            nameTag.innerHTML = name
-
-            const priceTag = document.createElement('div')
-            priceTag.classList.add("price")
-            priceTag.classList.add(`price${i}`)
-            priceTag.style.marginTop = "10px"
-            priceTag.style.fontWeight = "300"
-            priceTag.style.color = "#fffced"
-            priceTag.innerHTML = price
-
-            const image = document.createElement('div') 
-            image.classList.add("car-image")
-            image.style.backgroundImage = `url(${link})`
-
-            card.append(image)
-            card.append(nameTag)
-            card.append(priceTag)
-
-            const wrapper = document.querySelector(".wrapper")
-            wrapper.append(card)
-            console.log(card)
-            })
+        fetch('data.json')
+        .then((response) => {
+            return response.json();
         })
+        .then((data) => {
+            data.cars.map((item, i) => {
+                let price = item.price
+                let name = item.name
+                let link = item.URL
+                const card = document.createElement('div')
+                card.classList.add("card")
+                card.classList.add(`card${i}`)
+    
+                const nameTag = document.createElement('div')
+                nameTag.classList.add("name")
+                nameTag.classList.add(`name${i}`)
+                nameTag.style.marginTop = "10px"
+                nameTag.style.fontSize = "20px"
+                nameTag.style.fontWeight = "700"
+                nameTag.style.color = "#fffced"
+                nameTag.innerHTML = name
+    
+                const priceTag = document.createElement('div')
+                priceTag.classList.add("price")
+                priceTag.classList.add(`price${i}`)
+                priceTag.style.marginTop = "10px"
+                priceTag.style.fontWeight = "300"
+                priceTag.style.color = "#fffced"
+                priceTag.innerHTML = price
+    
+                const image = document.createElement('div') 
+                image.classList.add("car-image")
+                image.style.backgroundImage = `url(${link})`
+    
+                card.append(image)
+                card.append(nameTag)
+                card.append(priceTag)
+    
+                const wrapper = document.querySelector(".wrapper")
+                wrapper.append(card)
+                console.log(card)
+                })
+        });
+
+        
+        // })
 }
 
 requestCreation()
